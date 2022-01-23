62721b8f7ad9f3830d2393bd35183f25d65faefb
ksbeasle <ksbeasle@asu.edu>
2022-01-23 13:48:45 -0700

    small progress :beer:

--- ATM/ATM/Entities/BankAccountEntity.cs
+++ ATM/ATM/Entities/BankAccountEntity.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ATM.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +7,66 @@ using System.Threading.Tasks;
 
 namespace ATM.Entities
 {
-    public class BankAccountEntity
+    public class BankAccountEntity : IAccountActions
     {
-        public int AccountNumber { get; set; }
-        public BaseCardEntity[] Cards { get; set; }
-        public int MaxTransactionLimit { get; set; }
-        public int MaxCashWithdrawalLimit { get; set; }
-        public int BankPin { get; set; }
+        private Guid _AccountNumber;
+        public Guid AccountNumber
+        {
+            get { return _AccountNumber; }
+            set
+            {
+                _AccountNumber = value;
+            }
+        }
+        private BaseCardEntity[]? _Cards;
+        public BaseCardEntity[]? Cards
+        {
+            get { return _Cards; }
+            set
+            {
+                _Cards = value;
+            }
+        }
+        private float _MaxTransactionLimit;
+        public float MaxTransactionLimit
+        {
+            get { return _MaxTransactionLimit; }
+            set
+            {
+                _MaxTransactionLimit = value;
+            }
+        }
+        private float _MaxCashWithdrawalLimit;
+        public float MaxCashWithdrawalLimit
+        {
+            get { return _MaxCashWithdrawalLimit; }
+            set
+            {
+                _MaxCashWithdrawalLimit = value;
+            }
+        }
+        private int _BankPin;
+        public int BankPin
+        {
+            get { return _BankPin; }
+            set
+            {
+                _BankPin = value;
+            }
+        }
+        private float _Balance;
+        public float Balance
+        {
+            get { return _Balance; }
+            set
+            {
+                _Balance = value;
+            }
+        }
+
+        public float Withdrawal()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- ATM/ATM/Entities/BaseCardEntity.cs
+++ ATM/ATM/Entities/BaseCardEntity.cs
@@ -8,10 +8,51 @@ namespace ATM.Entities
 {
     public class BaseCardEntity
     {
-        public string CardOwnerName { get; set; }
-        public string CardType { get; set; }
-        public int CardNumber { get; set; }
-        public int CardSecurityCode { get; set; }
-        public DateTime CardExpiration { get; set; }
+        private string _CardOwnerName;
+
+        public string CardOwnerName
+        {
+            get { return _CardOwnerName; }
+            set
+            {
+                _CardOwnerName = value;
+            }
+        }
+        private string _CardType;
+        public string CardType
+        {
+            get { return _CardType; }
+            set
+            {
+                _CardType = value;
+            }
+        }
+        private int _CardNumber;
+        public int CardNumber
+        {
+            get { return _CardNumber; }
+            set
+            {
+                _CardNumber = value;
+            }
+        }
+        private int _CardSecurityCode;
+        public int CardSecurityCode
+        {
+            get { return _CardSecurityCode; }
+            set
+            {
+                _CardSecurityCode = value;
+            }
+        }
+        private DateTime _CardExpiration;
+        public DateTime CardExpiration
+        {
+            get { return CardExpiration; }
+            set
+            {
+                _CardExpiration = value;
+            }
+        }
     }
 }
