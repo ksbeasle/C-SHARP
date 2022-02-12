69977cd1d3eded48b20a8b4967385bd156b0b3d2
ksbeasle <ksbeasle@asu.edu>
2022-02-11 23:26:15 -0700

    working on adding a new card with a sql transaction

--- ATM/ATM/DB/DB.cs
+++ ATM/ATM/DB/DB.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Configuration;
 using System.Data;
 using System.Data.SqlClient;
+using System.Text;
 
 namespace ATM.DB
 {
@@ -38,17 +39,36 @@ namespace ATM.DB
             decimal balance = 0.0m;
             try
             {
-                SqlCommand command = new SqlCommand("INSERT INTO dbo.BankAccounts (accountNumber, username, pin, balance) " + "VALUES(@accountNumber, @username, @pin, @balance)", _connection);
+                // TODO finish transaction for both tables
+                SqlTransaction transaction;
+                transaction = _connection.BeginTransaction("NewBankAccountTransaction");
+
+                SqlCommand bankaccountInsert = new SqlCommand("INSERT INTO dbo.BankAccounts (accountNumber, username, pin, balance) " + "VALUES(@accountNumber, @username, @pin, @balance)", _connection);
 
                 // Add the parameters for the InsertCommand.
-                command.Parameters.AddWithValue("@accountNumber", accountNumber);
-                command.Parameters.AddWithValue("@username", username);
-                command.Parameters.AddWithValue("@pin", pin);
-                command.Parameters.AddWithValue("@balance", balance);
+                bankaccountInsert.Parameters.AddWithValue("@accountNumber", accountNumber);
+                bankaccountInsert.Parameters.AddWithValue("@username", username);
+                bankaccountInsert.Parameters.AddWithValue("@pin", pin);
+                bankaccountInsert.Parameters.AddWithValue("@balance", balance);
 
-                var res = command.ExecuteNonQuery();
-                return res;
+                var bankAccountRes = bankaccountInsert.ExecuteNonQuery();
 
+                int iter = 0;
+                StringBuilder sb = new StringBuilder("");
+                do
+                {
+                    int randomNumber = new Random().Next(1000, 9999);
+                    if(iter != 3)
+                    {
+                        sb.Append(randomNumber + "-");
+                    }else
+                    {
+                        sb.Append(randomNumber);
+                    }
+                    iter++;
+                } while (iter != 4);
+                return 1;
+                //SqlCommand newCard = new SqlCommand("INSERT INTO dbo.BankCards () " + "VALUES()");
             }
             catch (Exception e)
             {
